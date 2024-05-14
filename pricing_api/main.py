1e68f5e3b9bad3c50e14537e0c0aa8ca5e2df156
Philip Tempelman <tempelmanphilip@gmail.com>
2024-05-13 21:14:07 -0400

    clean pricing api and set interval to 30 minutes

--- pricing_api/main.py
+++ pricing_api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI
 import uvicorn
 from mangum import Mangum
 import requests
@@ -16,18 +16,6 @@ async def read_root():
     return {"message": "Welcome to the pricing API"}
 
 
-# @app.post("/get_price")
-# async def get_price(request: Request):
-#     # Request will have information: platform and drug_name
-#     pass
-
-
-# @app.post("/predict_price")
-# async def predict(request: Request):
-#     # Request will have information: platform and drug_name
-#     pass
-
-
 async def collect_and_send():
     url = "http://localhost:3000/api/data"
     while True:
@@ -41,13 +29,13 @@ async def collect_and_send():
 
         response = requests.post(url, json=data)
         print(f"Data posted, status: {response.status_code}")
-        await asyncio.sleep(10)  # updates every half hour
+        await asyncio.sleep(1800)  # updates every half hour
 
 
 @app.on_event("startup")
 async def start_scheduler():
     print("Starting scheduler")
-    scheduler.add_job(collect_and_send, "interval", seconds=10)  # , minute="*/30")
+    scheduler.add_job(collect_and_send, "interval", minute="*/30")
     scheduler.start()
 
 
